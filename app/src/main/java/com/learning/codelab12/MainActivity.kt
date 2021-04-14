c1b579b077cc4884ef6b267665bced5a72cd507d
Imran Baig <imran.baig@vipps.no>
2021-04-14 17:22:11 +0200

    task1

--- app/src/main/java/com/learning/codelab12/MainActivity.kt
+++ app/src/main/java/com/learning/codelab12/MainActivity.kt
@@ -2,39 +2,11 @@ package com.learning.codelab12
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.learning.codelab12.adapters.CountryListAdapter
-import com.learning.codelab12.databinding.ActivityMainBinding
-import com.learning.codelab12.models.CountryStats
-import com.learning.codelab12.viewmodels.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
-    private val viewModel = MainViewModel()
-    private var listAdapter = CountryListAdapter(ArrayList<CountryStats>())
-
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
-        var binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.recyclerView.layoutManager = LinearLayoutManager(this)
-        binding.recyclerView.adapter = listAdapter
-
-        viewModel.liveStats.observe(this, { newList->
-            listAdapter.update(newList)
-        })
-
-        viewModel.isLoading.observe(this, { loading->
-            binding.progressBar.visibility = if(loading) View.VISIBLE else View.INVISIBLE
-        })
-
-    }
-
-    override fun onResume() {
-        super.onResume()
-        viewModel.refresh()
+        setContentView(R.layout.activity_main)
     }
 }
--- app/src/main/java/com/learning/codelab12/models/CountryStats.kt
+++
@@ -1,3 +0,0 @@
-package com.learning.codelab12.models
-
- data class CountryStats (var Country: String, var CountryCode: String , var TotalConfirmed: Int)
