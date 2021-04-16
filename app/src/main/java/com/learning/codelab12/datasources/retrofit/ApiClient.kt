58d2196dc26864c3f3d33400c05fb65dceefd0ff
Imran Baig <imran.baig@vipps.no>
2021-04-16 16:29:01 +0200

    Update covid api

--- app/src/main/java/com/learning/codelab12/datasources/retrofit/ApiClient.kt
+++ app/src/main/java/com/learning/codelab12/datasources/retrofit/ApiClient.kt
@@ -7,26 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class ApiClient {
 
-
-    fun getSummary(): List<CountryStats> {
-       var result = api.getSummary().execute()
-
-        if(result.isSuccessful){
-            result.body()?.let { summaryResponse ->
-                return summaryResponse.Countries
-           }
-        }
-        return ArrayList<CountryStats>()
-    }
-
-    private var api: CovidApi
-
-    companion object{
-        val instance  : ApiClient by lazy {
-            ApiClient()
-        }
-    }
-
+    private var covidApi: CovidApi
     init {
         val gson = GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ss").create()
 
@@ -38,6 +19,17 @@ class ApiClient {
             .baseUrl("https://api.covid19api.com/")
             .build()
 
-        api = retrofit.create(CovidApi::class.java)
+        covidApi = retrofit.create(CovidApi::class.java)
+    }
+
+    fun getSummary(): List<CountryStats> {
+        var result = covidApi.getSummary().execute()
+
+        if(result.isSuccessful){
+            result.body()?.let { summaryResponse ->
+                return summaryResponse.Countries
+            }
+        }
+        return ArrayList<CountryStats>()
     }
 }
