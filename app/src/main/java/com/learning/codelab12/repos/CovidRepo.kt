89458bc7c779c40d3ddfa597745066dd1ceee988
Imran Baig <imran.baig@vipps.no>
2021-04-18 20:33:50 +0200

    task 5

--- app/src/main/java/com/learning/codelab12/repos/CovidRepo.kt
+++ app/src/main/java/com/learning/codelab12/repos/CovidRepo.kt
@@ -1,17 +1,15 @@
 package com.learning.codelab12.repos
 
-import com.learning.codelab12.datasources.retrofit.CovidApiClient
 import com.learning.codelab12.datasources.LiveSource
 import com.learning.codelab12.models.CountryStats
 
 class CovidRepo {
 
-    private val covidApiClient = CovidApiClient()
     private val liveSource  = LiveSource()
 
 
     suspend fun getCountriesSummary() : List<CountryStats>  {
-        var list  = covidApiClient.getSummary()
+        var list  = liveSource.getSummary()
         return list.sortedByDescending {it.TotalConfirmed}
     }
 
