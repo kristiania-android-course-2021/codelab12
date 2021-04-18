d891e0f942e3b6e4f557f3f2cc440dd14796bc03
Imran Baig <imran.baig@vipps.no>
2021-04-18 20:16:13 +0200

    task 1

--- app/src/main/java/com/learning/codelab12/MainActivity.kt
+++ app/src/main/java/com/learning/codelab12/MainActivity.kt
@@ -2,17 +2,9 @@ package com.learning.codelab12
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.learning.codelab12.adapters.CountryListAdapter
 import com.learning.codelab12.databinding.ActivityMainBinding
-import com.learning.codelab12.datasources.DummySource
-import com.learning.codelab12.models.CountryStats
-import com.learning.codelab12.viewmodels.MainViewModel
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var listAdapter : CountryListAdapter
-    private val viewModel = MainViewModel()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,19 +13,5 @@ class MainActivity : AppCompatActivity() {
         var binding  = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        listAdapter = CountryListAdapter(ArrayList())
-
-        binding.recyclerView.layoutManager = LinearLayoutManager(this)
-        binding.recyclerView.adapter = listAdapter
-
-        viewModel.liveStats.observe(this, { list->
-            listAdapter.update(list)
-        })
     }
-
-    override fun onResume() {
-        super.onResume()
-        viewModel.refresh()
-    }
-
 }
